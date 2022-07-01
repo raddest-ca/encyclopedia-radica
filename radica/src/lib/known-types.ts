be3157a307808803e792268a0dff9abef9bc61de
TeamDman <TeamDman9201@gmail.com>
2022-07-01 13:19:34 -0400

    progress

--- radica/src/lib/known-types.ts
+++ radica/src/lib/known-types.ts
@@ -1,10 +1,18 @@
+const version = "1.0.0";
+
+// prettier-ignore
 export const knownTypes = {
-	meme: { id: "meme", version: "1.0.0" },
-	character: { id: "cartoon character", version: "1.0.0" },
-	name: { id: "nickname", version: "1.0.0" },
-	language: { id: "language", version: "1.0.0" },
-	literal: { id: "literal", version: "1.0.0" },
-	transcript: { id: "transcript", version: "1.0.0" },
-	feature: {id: "feature", version: "1.0.0"},
-	style: {id: "style", version: "1.0.0"},
+	type:			{ id: "type", 					version },
+	identifier:		{ id: "id", 					version },
+	hash:			{ id: "hash", 					version },
+	meme:			{ id: "meme", 					version },
+	character:		{ id: "cartoon character", 		version },
+	name:			{ id: "nickname", 				version },
+	language:		{ id: "language", 				version },
+	literal:		{ id: "literal", 				version },
+	transcript:		{ id: "transcript", 			version },
+	feature:		{ id: "feature", 				version },
+	style:			{ id: "style", 					version },
+	transformer:	{ id: "transformer", 			version },
+	jsfunc:			{ id: "javascript function", 	version },
 };
