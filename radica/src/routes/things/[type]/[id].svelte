d719c69a902db1d5e37ac528baacc505d61ad65f
TeamDman <TeamDman9201@gmail.com>
2022-07-17 20:21:57 -0400

    clean up not-found page

--- radica/src/routes/things/[type]/[id].svelte
+++ radica/src/routes/things/[type]/[id].svelte
@@ -2,5 +2,8 @@
 	import { _ } from "svelte-i18n";
 </script>
 
-<h1>There is no renderer available for this type.</h1>
-<a href="#" on:click={() => history.back()}>{$_("action.browser.back")}</a>
+<div class="container w-96 border-xl p-4 mt-4 m-auto bg-base-300">
+	<h1>There is no renderer available for this type.</h1>
+	<br />
+	<button class="link" on:click={() => history.back()}>{$_("action.browser.back")}</button>
+</div>
