9ee41e328ba9033fc7a424c9921f1eb3f48078e4
TeamDman <TeamDman9201@gmail.com>
2022-07-25 14:32:19 -0400

    fix type list and center it

--- front/src/routes/things/type/index.svelte
+++ front/src/routes/things/type/index.svelte
@@ -4,9 +4,7 @@
 	export async function load({ fetch, params }: LoadEvent): Promise<LoadOutput> {
 		const things = await getThings(fetch, {
 			filter: {
-				type: {
-					id: knownTypes.type.id,
-				},
+				type: knownTypes.type,
 			},
 		});
 
@@ -14,9 +12,7 @@
 			things.values!.map((x) =>
 				getThings(fetch, {
 					filter: {
-						type: {
-							id: x.id,
-						},
+						type: x.id as KnownType,
 					},
 					countOnly: true,
 				}),
@@ -27,9 +23,7 @@
 			things.values!.map((x) =>
 				getRelationships(fetch, {
 					filter: {
-						type: {
-							id: x.id,
-						},
+						type: x.id as KnownType,
 					},
 					countOnly: true,
 				}),
@@ -56,14 +50,14 @@
 		type ThingResults,
 	} from "$lib/requesting";
 	import { _ } from "svelte-i18n";
-	import { knownTypes } from "$lib/known-types";
+	import { knownTypes, type KnownType } from "$lib/known-types";
 	import type { Thing } from "$lib/core";
 
 	export let things: ThingResults;
 	export let thingCounts: ThingResults[];
 	export let relCounts: RelationshipResults[];
 
-	function getHref(thing: Thing) {
+	function getHref(thing: Thing<KnownType>) {
 		// since going in an href attrib as-is, needs to be sanitized twice.
 		const rtn = `./type/${encodeURIComponent(thing.id)}/`;
 		return rtn;
@@ -71,7 +65,7 @@
 </script>
 
 <main
-	class="place-content-center drop-shadow-2xl m-auto mt-10 rounded-xl p-4 bg-base-200"
+	class="place-content-center flex flex-wrap drop-shadow-2xl m-auto mt-10 rounded-xl p-4 bg-base-200"
 	style="width: 600px;"
 >
 	<p>
@@ -84,11 +78,7 @@
 	<table class="mt-2">
 		<thead>
 			<tr>
-				<th id="type" colspan="2">{$_("type")}</th>
-			</tr>
-			<tr>
-				<th id="typeid">{$_("id")}</th>
-				<th id="typeversion">{$_("version")}</th>
+				<th>{$_("type")}</th>
 				<th>{$_("id")}</th>
 				<th>{$_("count.things")}</th>
 				<th>{$_("count.relationships")}</th>
@@ -97,8 +87,7 @@
 		<tbody>
 			{#each things.values as thing, i}
 				<tr>
-					<td>{thing.type.id}</td>
-					<td>{thing.type.version}</td>
+					<td>{thing.type}</td>
 					<td><a class="link" href={getHref(thing)}>{thing.id}</a></td>
 					<td>{thingCounts[i].count}</td>
 					<td>{relCounts[i].count}</td>
@@ -115,11 +104,5 @@
 	td,
 	th {
 		@apply m-2 p-2 border-2 border-primary;
-	}
-
-	#type,
-	#typeid,
-	#typeversion {
-		@apply bg-primary-focus;
 	}
 </style>
